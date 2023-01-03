13056c836077ac21f12a5c4c26e43d1e982a699a
Ryo <9062624+gwanryo@users.noreply.github.com>
2023-01-03 23:30:17 +0900

    refac: remove LottoPrize to reduce complexity

--- src/main/java/model/LottoResult.java
+++ src/main/java/model/LottoResult.java
@@ -1,25 +1,43 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoResult {
+    private static final LinkedHashMap<LottoScore, Long> prize = new LinkedHashMap<>() {{
+        put(new LottoScore(3, false), 5000L);
+        put(new LottoScore(4, false), 50_000L);
+        put(new LottoScore(5, false), 1_500_000L);
+        put(new LottoScore(5, true), 30_000_000L);
+        put(new LottoScore(6, false), 2_000_000_000L);
+    }};
+
     private final LottoGame lottoGame;
     private final LottoDraw lottoDraw;
-    private final LottoPrize lottoPrize;
 
     public LottoResult(LottoGame lottoGame, LottoDraw lottoDraw) {
         this.lottoGame = lottoGame;
         this.lottoDraw = lottoDraw;
-        this.lottoPrize = new LottoPrize();
+    }
+
+    private String formatPrize(LottoScore lottoScore) {
+        if (lottoScore.getMatchCount() == 5 && lottoScore.isMatchBonus()) {
+            return String.format("%d개 일치, 보너스 볼 일치 (%d원) - ", lottoScore.getMatchCount(), getPrize(lottoScore));
+        }
+        return String.format("%d개 일치 (%d원) - ", lottoScore.getMatchCount(), getPrize(lottoScore));
+    }
+
+    private Long getPrize(LottoScore lottoScore) {
+        if (lottoScore.getMatchCount() == 5) {
+            return prize.getOrDefault(lottoScore, 0L);
+        }
+        return prize.getOrDefault(new LottoScore(lottoScore.getMatchCount(), false), 0L);
     }
 
     private long getTotalPrize() {
-        return lottoGame.getLottoList().stream().mapToLong(l -> lottoPrize.getPrize(lottoDraw.getScore(l))).sum();
-    }
-
-    private List<LottoScore> getLottoScores() {
-        return lottoGame.getLottoList().stream().map(lottoDraw::getScore).collect(Collectors.toList());
+        return lottoGame.getLottoList().stream().mapToLong(l -> getPrize(lottoDraw.getScore(l))).sum();
     }
 
     public double getEarningRate(long money) {
@@ -27,6 +45,12 @@ public class LottoResult {
     }
 
     public String getResult() {
-        return this.lottoPrize.formatPrizes(getLottoScores());
+        List<LottoScore> lottoScores = lottoGame.getLottoList().stream().map(lottoDraw::getScore).collect(Collectors.toList());
+        List<String> prizeResults = new ArrayList<>();
+        prize.forEach((lottoScore, prizeMoney) -> {
+            long count = lottoScores.stream().filter(l -> l.compare(lottoScore)).count();
+            prizeResults.add(formatPrize(lottoScore) + count + "개");
+        });
+        return String.join("\n", prizeResults);
     }
 }
--- src/test/java/model/LottoResultTest.java
+++ src/test/java/model/LottoResultTest.java
@@ -12,16 +12,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoResultTest {
     @ParameterizedTest
-    @MethodSource("generateLottosResults")
+    @MethodSource("generateLottoGameEarningRateData")
     @DisplayName("로또 수익률 계산")
-    void lottosResults(List<List<Integer>> lottosList, LottoDraw lottoDraw, long money, double expectedEarningRate) {
+    void lottoGameEarningRate(List<List<Integer>> lottosList, LottoDraw lottoDraw, long money, double expectedEarningRate) {
         LottoGame lottoGame = new LottoGame(createRandomNumbers(lottosList));
         lottoGame.auto(lottosList.size());
         LottoResult lottoResult = new LottoResult(lottoGame, lottoDraw);
         assertThat(lottoResult.getEarningRate(money)).isEqualTo(expectedEarningRate);
     }
 
-    static Stream<Arguments> generateLottosResults() {
+    static Stream<Arguments> generateLottoGameEarningRateData() {
         return Stream.of(
                 Arguments.of(
                         List.of(List.of(1, 2, 3, 4, 5, 6), List.of(7, 8, 9, 10, 11, 12)),
@@ -46,6 +46,41 @@ public class LottoResultTest {
                         new LottoDraw("1, 2, 3, 4, 5, 6", 7),
                         2000L,
                         (double) (5_000L + 50_000L) / 2000L
+                ) // 3개 일치, 4개 일치
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("generatelottoGameResultData")
+    @DisplayName("로또 결과 출력")
+    void lottoGameResult(List<List<Integer>> lottosList, LottoDraw lottoDraw, String expectedResult) {
+        LottoGame lottoGame = new LottoGame(createRandomNumbers(lottosList));
+        lottoGame.auto(lottosList.size());
+        LottoResult lottoResult = new LottoResult(lottoGame, lottoDraw);
+        assertThat(lottoResult.getResult()).isEqualTo(expectedResult);
+    }
+
+    static Stream<Arguments> generatelottoGameResultData() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(List.of(1, 2, 3, 4, 5, 6), List.of(7, 8, 9, 10, 11, 12)),
+                        new LottoDraw("1, 2, 3, 4, 5, 6", 7),
+                        "3개 일치 (5000원) - 0개\n4개 일치 (50000원) - 0개\n5개 일치 (1500000원) - 0개\n5개 일치, 보너스 볼 일치 (30000000원) - 0개\n6개 일치 (2000000000원) - 1개"
+                ), // 6개 일치
+                Arguments.of(
+                        List.of(List.of(1, 2, 3, 4, 5, 6), List.of(1, 2, 3, 4, 5, 6)),
+                        new LottoDraw("1, 2, 3, 4, 5, 6", 7),
+                        "3개 일치 (5000원) - 0개\n4개 일치 (50000원) - 0개\n5개 일치 (1500000원) - 0개\n5개 일치, 보너스 볼 일치 (30000000원) - 0개\n6개 일치 (2000000000원) - 2개"
+                ), // 6개 일치 + 6개 일치
+                Arguments.of(
+                        List.of(List.of(5, 4, 3, 2, 1, 9), List.of(3, 5, 6, 7, 2, 4)),
+                        new LottoDraw("1, 2, 3, 4, 5, 6", 7),
+                        "3개 일치 (5000원) - 0개\n4개 일치 (50000원) - 0개\n5개 일치 (1500000원) - 1개\n5개 일치, 보너스 볼 일치 (30000000원) - 1개\n6개 일치 (2000000000원) - 0개"
+                ), // 5개 일치 + 5개 일치, 보너스 일치
+                Arguments.of(
+                        List.of(List.of(1, 2, 3, 34, 35, 36), List.of(1, 2, 3, 4, 35, 36)),
+                        new LottoDraw("1, 2, 3, 4, 5, 6", 7),
+                        "3개 일치 (5000원) - 1개\n4개 일치 (50000원) - 1개\n5개 일치 (1500000원) - 0개\n5개 일치, 보너스 볼 일치 (30000000원) - 0개\n6개 일치 (2000000000원) - 0개"
                 ) // 3개 일치, 4개 일치
         );
     }
